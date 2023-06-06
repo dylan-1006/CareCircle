9d6f048745bcf05647ffb72c749daade538e1583
sexxyrexxy <rexlim2003@gmail.com>
2023-06-06 13:28:37 +0800

    classes

---
+++ src/main/java/carecircle/classes/nurse.java
@@ -0,0 +1,18 @@
+package carecircle.classes;
+
+public class nurse {
+    public String nurseID;
+    public String name;
+    public String email;
+    public String dateOfBirth;
+    public String gender;
+
+    public nurse(String nurseID, String name, String phoneNo, String email, String dateOfBirth, String gender) {
+        this.nurseID=nurseID;
+        this.name=name;
+        this.email=email;
+        this.dateOfBirth=dateOfBirth;
+        this.gender=gender;
+
+    }
+}
