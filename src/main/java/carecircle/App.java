1190e725599d432d56ada20fd0636e4ecb8e47e3
Dylan <eu.dylan@gmail.com>
2023-06-08 18:06:06 +0800

    ok trial 1

--- src/main/java/carecircle/App.java
+++ src/main/java/carecircle/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("loginScreen"), 1200, 600);
+        scene = new Scene(loadFXML("patientScreenGeneral"), 1200, 600);
         // stage.setFullScreen(true);
         stage.setScene(scene);
         stage.setResizable(false);
