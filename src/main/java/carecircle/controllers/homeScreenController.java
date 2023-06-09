5b266859c6793bd5d59bbcd3f4968b73ea9643ca
Dylan <eu.dylan@gmail.com>
2023-06-09 23:26:04 +0800

    added navigation controls for home screen side bar

--- src/main/java/carecircle/controllers/homeScreenController.java
+++ src/main/java/carecircle/controllers/homeScreenController.java
@@ -1,6 +1,10 @@
 package carecircle.controllers;
 
+import java.io.IOException;
+
+import carecircle.App;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -10,6 +14,30 @@ public class homeScreenController {
 
     @FXML
     private Pane backgroundPane;
+
+    @FXML
+    private Pane navigationSideBar;
+
+    @FXML
+    private Pane sideBarAppointmentButton;
+
+    @FXML
+    private Pane sideBarDashboardButton;
+
+    @FXML
+    private Pane sideBarHelpCenterButton;
+
+    @FXML
+    private Pane sideBarLogoutButton;
+
+    @FXML
+    private Pane sideBarMedicalStuffButton;
+
+    @FXML
+    private Pane sideBarPatientButton;
+
+    @FXML
+    private Pane sideBarSettingsButton;
 
     @FXML
     private VBox menuBar;
@@ -22,5 +50,48 @@ public class homeScreenController {
 
     @FXML
     private Text titleText2;
+
+    @FXML
+    void switchToAppoitmentScreen(MouseEvent event) throws IOException {
+        App.setRoot("appointmentScreenGeneral");
+
+    }
+
+    @FXML
+    void switchToHelpCenterScreen(MouseEvent event) throws IOException {
+        App.setRoot("helpCenterScreen");
+
+    }
+
+    @FXML
+    void switchToHomeScreen(MouseEvent event) throws IOException {
+        App.setRoot("homeScreen");
+
+    }
+
+    @FXML
+    void switchToLoginScreen(MouseEvent event) throws IOException {
+        App.setRoot("loginScreen");
+
+    }
+
+    @FXML
+    void switchToMedicalStaffScreen(MouseEvent event) throws IOException {
+        App.setRoot("medicalStaffScreenGeneral");
+
+    }
+
+    @FXML
+    void switchToPatientScreen(MouseEvent event) throws IOException {
+        App.setRoot("patientScreenGeneral");
+
+    }
+
+    @FXML
+    void switchToSettingsScreen(MouseEvent event) throws IOException {
+        // Have to merge settings screen with master first
+        // App.setRoot("appointmentScreenGeneral");
+
+    }
 
 }
