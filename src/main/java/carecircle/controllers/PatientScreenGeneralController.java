2b1489d92a7bcec48fcc4ae60484e3d3403666eb
Dylan <eu.dylan@gmail.com>
2023-06-12 22:29:25 +0800

    Added table for patients screen

--- src/main/java/carecircle/controllers/PatientScreenGeneralController.java
+++ src/main/java/carecircle/controllers/PatientScreenGeneralController.java
@@ -3,14 +3,27 @@ package carecircle.controllers;
 import java.io.IOException;
 
 import carecircle.App;
+import carecircle.classes.patient;
+import carecircle.data.patientData;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import carecircle.tableModels.patientTableModel;
 
 public class PatientScreenGeneralController {
+
+    @FXML
+    private Text totalPatientAdmissions;
+
+    @FXML
+    private TableView<patientTableModel> patientScreenTable;
 
     @FXML
     private VBox menuBar;
@@ -47,6 +60,48 @@ public class PatientScreenGeneralController {
 
     @FXML
     private Text titleText2;
+
+    public void initialize() {
+
+        // Loading the patient table
+        ObservableList<patientTableModel> patientDataList = patientTableModel
+                .convertPatientDataToPatientDataModel();
+
+        TableColumn patientIdColumn = new TableColumn("Patient ID");
+        TableColumn patientNameColumn = new TableColumn<>("Patient Name");
+        TableColumn icColumn = new TableColumn("IC");
+        TableColumn phoneNoColumn = new TableColumn("Phone No");
+        TableColumn dateOfBirthColumn = new TableColumn("Date of Birth");
+        TableColumn heightColumn = new TableColumn("Height [CM]");
+        TableColumn weightColumn = new TableColumn("Weight [KG]");
+        TableColumn bloodTypeColumn = new TableColumn("Blood Type");
+
+        TableColumn detailsColumn = new TableColumn("");
+
+        patientScreenTable.getColumns().addAll(patientIdColumn, patientNameColumn, icColumn, phoneNoColumn,
+                dateOfBirthColumn, heightColumn, weightColumn, bloodTypeColumn, detailsColumn);
+
+        patientIdColumn.setCellValueFactory(new PropertyValueFactory<>("patientId"));
+        patientNameColumn.setCellValueFactory(new PropertyValueFactory<>("patientName"));
+        icColumn.setCellValueFactory(new PropertyValueFactory<>("ic"));
+        phoneNoColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNo"));
+        dateOfBirthColumn.setCellValueFactory(new PropertyValueFactory<>("dateOfBirth"));
+        heightColumn.setCellValueFactory(new PropertyValueFactory<>("height"));
+        weightColumn.setCellValueFactory(new PropertyValueFactory<>("weight"));
+        bloodTypeColumn.setCellValueFactory(new PropertyValueFactory<>("bloodType"));
+
+        detailsColumn.setCellValueFactory(new PropertyValueFactory<>("details"));
+
+        patientScreenTable.setItems(patientDataList);
+
+        setPatientAmount();
+
+    }
+
+    void setPatientAmount(){
+            totalPatientAdmissions.setText(Integer.toString(patientData.loadPatientDataFromDatabase().size()));
+
+    }
 
     @FXML
     void switchToAppoitmentScreen(MouseEvent event) throws IOException {
