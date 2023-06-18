49edcb90868964d5b586dd2f773305fe5e5952a5
Dylan <eu.dylan@gmail.com>
2023-06-19 01:54:22 +0800

    Added side bar details for all patient panels

--- src/main/java/carecircle/controllers/PatientDetailsScreenDiagnosisController.java
+++ src/main/java/carecircle/controllers/PatientDetailsScreenDiagnosisController.java
@@ -1,12 +1,20 @@
 package carecircle.controllers;
+
 import java.io.IOException;
+import java.time.Year;
+import java.util.Optional;
 
 import carecircle.App;
+import carecircle.data.patientData;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -70,7 +78,48 @@ public class PatientDetailsScreenDiagnosisController {
     private TextField weight;
 
     @FXML
-    void deletePatient(ActionEvent event) {
+    private Pane patientDetailsSidePane;
+
+    public void initialize() {
+
+        setSideBarPatientDetails();
+    }
+
+    void setSideBarPatientDetails() {
+
+        // Calculating the patient age
+        int year = Year.now().getValue();
+        int patientAge = year - Integer.parseInt(patientData.initPatientData.getDateOfBirth().substring(0, 4));
+
+        // Setting the patient details
+        patientName.setText(patientData.initPatientData.getName());
+        patientID.setText(patientData.initPatientData.getPatientID());
+        gender.setText(patientData.initPatientData.getGender());
+        age.setText(Integer.toString(patientAge));
+        height.setText(Double.toString(patientData.initPatientData.getHeight()) + "cm");
+        weight.setText(Double.toString(patientData.initPatientData.getWeight()) + "kg");
+        bloodType.setText(patientData.initPatientData.getBloodType());
+
+    }
+
+    @FXML
+    void deleteThisPatient(ActionEvent event) throws IOException {
+
+        Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmation.setTitle("Confirmation");
+        confirmation.setHeaderText("Are you sure you want to proceed?");
+        confirmation.setContentText("Click OK to continue or Cancel to abort.");
+
+        Optional<ButtonType> result = confirmation.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+
+            patientData.deletePatient(patientData.initPatientData.getPatientID());
+            App.setRoot("patientScreenGeneral");
+        } else {
+
+            App.setRoot("patientGeneralDetailsScreen");
+        }
 
     }
 
@@ -85,12 +134,12 @@ public class PatientDetailsScreenDiagnosisController {
     }
 
     @FXML
-    void switchToAnalysisSection(ActionEvent event) throws IOException{
+    void switchToAnalysisSection(ActionEvent event) throws IOException {
         App.setRoot("patientDetailsScreenAnalysis");
     }
 
     @FXML
-    void switchToGeneralSection(ActionEvent event) throws IOException{
+    void switchToGeneralSection(ActionEvent event) throws IOException {
         App.setRoot("patientGeneralDetailsScreen");
     }
 
@@ -100,8 +149,15 @@ public class PatientDetailsScreenDiagnosisController {
     }
 
     @FXML
-    void switchToTreatmentSection(ActionEvent event) throws IOException{
+    void switchToTreatmentSection(ActionEvent event) throws IOException {
         App.setRoot("patientDetailsScreenTreatment");
+    }
+
+    @FXML
+    void switchToPatient(MouseEvent event) throws IOException {
+
+        App.setRoot("patientScreenGeneral");
+
     }
 
 }
--- src/main/java/carecircle/controllers/PatientDetailsScreenMedicalHistoryController.java
+++ src/main/java/carecircle/controllers/PatientDetailsScreenMedicalHistoryController.java
@@ -1,16 +1,26 @@
 package carecircle.controllers;
+
 import java.io.IOException;
+import java.time.Year;
+import java.util.Optional;
 
 import carecircle.App;
+import carecircle.data.patientData;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class PatientDetailsScreenMedicalHistoryController {
+
+    @FXML
+    private TextField patientName;
 
     @FXML
     private Button addMedicalHistoryButton;
@@ -66,34 +76,84 @@ public class PatientDetailsScreenMedicalHistoryController {
     @FXML
     private TextField weight;
 
+    public void initialize() {
+
+        setSideBarPatientDetails();
+    }
+
+    void setSideBarPatientDetails() {
+
+        // Calculating the patient age
+        int year = Year.now().getValue();
+        int patientAge = year - Integer.parseInt(patientData.initPatientData.getDateOfBirth().substring(0, 4));
+
+        // Setting the patient details
+        patientName.setText(patientData.initPatientData.getName());
+        patientID.setText(patientData.initPatientData.getPatientID());
+        gender.setText(patientData.initPatientData.getGender());
+        age.setText(Integer.toString(patientAge));
+        height.setText(Double.toString(patientData.initPatientData.getHeight()) + "cm");
+        weight.setText(Double.toString(patientData.initPatientData.getWeight()) + "kg");
+        bloodType.setText(patientData.initPatientData.getBloodType());
+
+    }
+
+    @FXML
+    void deleteThisPatient(ActionEvent event) throws IOException {
+
+        Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmation.setTitle("Confirmation");
+        confirmation.setHeaderText("Are you sure you want to proceed?");
+        confirmation.setContentText("Click OK to continue or Cancel to abort.");
+
+        Optional<ButtonType> result = confirmation.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+
+            patientData.deletePatient(patientData.initPatientData.getPatientID());
+            App.setRoot("patientScreenGeneral");
+        } else {
+
+            App.setRoot("patientGeneralDetailsScreen");
+        }
+
+    }
+
     @FXML
     void editMedicalHistoryRecord(ActionEvent event) {
 
     }
 
     @FXML
-    void switchToAddMedicalHistoryScreen(ActionEvent event) throws IOException{
-        
+    void switchToAddMedicalHistoryScreen(ActionEvent event) throws IOException {
+
     }
 
     @FXML
-    void switchToAnalysisSection(ActionEvent event) throws IOException{
+    void switchToAnalysisSection(ActionEvent event) throws IOException {
         App.setRoot("patientDetailsScreenAnalysis");
     }
 
     @FXML
-    void switchToDiagnosisSection(ActionEvent event) throws IOException{
+    void switchToDiagnosisSection(ActionEvent event) throws IOException {
         App.setRoot("patientDetailsScreenDiagnosis");
     }
 
     @FXML
-    void switchToGeneralSection(ActionEvent event) throws IOException{
+    void switchToGeneralSection(ActionEvent event) throws IOException {
         App.setRoot("patientGeneralDetailsScreen");
     }
 
     @FXML
-    void switchToTreatmentSection(ActionEvent event) throws IOException{
+    void switchToTreatmentSection(ActionEvent event) throws IOException {
         App.setRoot("patientDetailsScreenTreatment");
+    }
+
+    @FXML
+    void switchToPatient(MouseEvent event) throws IOException {
+
+        App.setRoot("patientScreenGeneral");
+
     }
 
 }
