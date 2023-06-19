b990672f873541c6b597e2b573728b87fd38e7a4
Dylan <eu.dylan@gmail.com>
2023-06-19 14:55:50 +0800

    Added missing classes

--- src/main/java/carecircle/classes/medicalHistory.java
+++ src/main/java/carecircle/classes/medicalHistory.java
@@ -1,5 +1,13 @@
 package carecircle.classes;
 
 public class medicalHistory {
-   
+
+    private String patientId;
+    private String diagnosisId;
+    private String treatmentId;
+    private String procedureId;
+    private String description;
+    private String allergies;
+    private String pastMedicationId;
+
 }
---
+++ src/main/java/carecircle/classes/procedure.java
@@ -0,0 +1,11 @@
+package carecircle.classes;
+
+public class procedure {
+
+    private String procedureId;
+    private String description;
+    private String date;
+    private String doctorId;
+    private String patientId;
+
+}
