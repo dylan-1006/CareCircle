03b3c8bf7401e62f9838ad6dae1cc62cf59c8777
Dylan <eu.dylan@gmail.com>
2023-06-19 14:59:44 +0800

    added constructor, getters & setters for 3 classes

--- src/main/java/carecircle/classes/medicalHistory.java
+++ src/main/java/carecircle/classes/medicalHistory.java
@@ -10,4 +10,70 @@ public class medicalHistory {
     private String allergies;
     private String pastMedicationId;
 
+    public medicalHistory(String patientId, String diagnosisId, String treatmentId, String procedureId,
+            String description, String allergies, String pastMedicationId) {
+        this.patientId = patientId;
+        this.diagnosisId = diagnosisId;
+        this.treatmentId = treatmentId;
+        this.procedureId = procedureId;
+        this.description = description;
+        this.allergies = allergies;
+        this.pastMedicationId = pastMedicationId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getDiagnosisId() {
+        return diagnosisId;
+    }
+
+    public void setDiagnosisId(String diagnosisId) {
+        this.diagnosisId = diagnosisId;
+    }
+
+    public String getTreatmentId() {
+        return treatmentId;
+    }
+
+    public void setTreatmentId(String treatmentId) {
+        this.treatmentId = treatmentId;
+    }
+
+    public String getProcedureId() {
+        return procedureId;
+    }
+
+    public void setProcedureId(String procedureId) {
+        this.procedureId = procedureId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getPastMedicationId() {
+        return pastMedicationId;
+    }
+
+    public void setPastMedicationId(String pastMedicationId) {
+        this.pastMedicationId = pastMedicationId;
+    }
 }
--- src/main/java/carecircle/classes/medicine.java
+++ src/main/java/carecircle/classes/medicine.java
@@ -1,13 +1,37 @@
 package carecircle.classes;
 
 public class medicine {
-    public String medicineID;
-    public int amountLeft;
-    public double dosage;
+    private String medicineID;
+    private int amountLeft;
+    private double dosage;
 
-    public medicine(String medicineID,int amountLeft,double dosage){
-        this.medicineID=medicineID;
-        this.amountLeft=amountLeft;
-        this.dosage=dosage;
+    public medicine(String medicineID, int amountLeft, double dosage) {
+        this.medicineID = medicineID;
+        this.amountLeft = amountLeft;
+        this.dosage = dosage;
+    }
+
+    public String getMedicineID() {
+        return medicineID;
+    }
+
+    public void setMedicineID(String medicineID) {
+        this.medicineID = medicineID;
+    }
+
+    public int getAmountLeft() {
+        return amountLeft;
+    }
+
+    public void setAmountLeft(int amountLeft) {
+        this.amountLeft = amountLeft;
+    }
+
+    public double getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(double dosage) {
+        this.dosage = dosage;
     }
 }
