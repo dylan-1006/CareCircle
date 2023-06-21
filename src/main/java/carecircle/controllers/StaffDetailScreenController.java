da984124a8511b0747a6df4473b88fd20281c9c5
sexxyrexxy <rexlim2003@gmail.com>
2023-06-21 14:30:32 +0800

    deletgeAppointment

---
+++ src/main/java/carecircle/controllers/StaffDetailScreenController.java
@@ -0,0 +1,54 @@
+package carecircle.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class StaffDetailScreenController {
+
+    @FXML
+    private TextField contactNumber;
+
+    @FXML
+    private TextField dateOfBirth;
+
+    @FXML
+    private Text deleteStaffButton;
+
+    @FXML
+    private TextField department;
+
+    @FXML
+    private TextField doctorID;
+
+    @FXML
+    private Text editButton;
+
+    @FXML
+    private TextField gender;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Text sideAge;
+
+    @FXML
+    private Text sideName;
+
+    @FXML
+    private Text sideGender;
+
+    @FXML
+    private Text sideStaffID;
+
+    @FXML
+    private Text staffID;
+
+    @FXML
+    void deleteThisStaff(MouseEvent event) {
+
+    }
+
+}
